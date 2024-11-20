c303140e376983250395646fa6b108a36b8eda6f
FaronicsProServices <proservices@faronics.com>
2024-11-21 00:05:34 +0530

    Update DisableSpecificUserAccount.ps1

--- DisableSpecificUserAccount.ps1
+++ DisableSpecificUserAccount.ps1
@@ -1,3 +1,10 @@
 # Disable a specific local user account on the system
 # Replace "Username" with the name of the user account you want to disable
 Disable-LocalUser -Name "Username"
+
+# Example:
+# To enable the account named "JakeAdams", use:
+# Enable-LocalUser -Name "JakeAdams"
+
+# This command allows the specified user to log in again.
+# Keep in mind, you need admin privileges to run this command successfully.
