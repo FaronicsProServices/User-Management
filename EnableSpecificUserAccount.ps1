337111565b28838137a6fad6e0d2bfa0991b85ec
FaronicsProServices <proservices@faronics.com>
2024-11-21 00:05:16 +0530

    Create EnableSpecificUserAccount.ps1

---
+++ EnableSpecificUserAccount.ps1
@@ -0,0 +1,10 @@
+# Specify the username of the local account you want to enable
+# Replace "Username" with the actual account name
+Enable-LocalUser -Name "Username"
+
+# Example:
+# To enable the account named "JakeAdams", use:
+# Enable-LocalUser -Name "JakeAdams"
+
+# This command allows the specified user to log in again.
+# Keep in mind, you need admin privileges to run this command successfully.
