4e3aba8e9031b812e619c651526b13270a727664
FaronicsProServices <proservices@faronics.com>
2024-11-21 21:30:41 +0530

    Create AutomaticUserLogin.ps1

---
+++ AutomaticUserLogin.ps1
@@ -0,0 +1,14 @@
+# This script enables automatic logon for a specified user, disables user switch at login, 
+# and sets the monitor and standby timeouts to 0. It then forces a restart of the computer.
+$Username = "Username" 
+$Password = "Password" 
+$RegPath = "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" 
+Set-ItemProperty -Path $RegPath -Name "AutoAdminLogon" -Value 1 
+Set-ItemProperty -Path $RegPath -Name "DefaultUsername" -Value $Username 
+Set-ItemProperty -Path $RegPath -Name "DefaultPassword" -Value $Password 
+New-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Authentication\LogonUI" -Name "LogonUserSwitch" -Value 0 -Force 
+powercfg -change -monitor-timeout-ac 0 
+powercfg -change -monitor-timeout-dc 0 
+powercfg -change -standby-timeout-ac 0 
+powercfg -change -standby-timeout-dc 0 
+Restart-Computer -Force
